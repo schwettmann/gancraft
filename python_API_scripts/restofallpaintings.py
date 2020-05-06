37967adb3a250f8002e65b5f59138779e493b0df
Anisha Agarwal <anisha24@mit.edu>
2020-05-06 01:05:48 -0400

    new attempt: ref_urls from wiki

--- python_API_scripts/restofallpaintings.py
+++ python_API_scripts/restofallpaintings.py
@@ -77,8 +77,16 @@ def download_file_from_url(file_url, file_name, save_directory=None):
     file_path = os.path.join(working_directory, file_name)
     if os.path.exists(file_path):
         os.remove(file_path)
-
-    file_path, _ = urllib.request.urlretrieve(file_url, file_path)
+    try:
+        file_path, _ = urllib.request.urlretrieve(file_url, file_path)
+    except Exception as inst:
+        if str(inst) == "HTTP Error 403: Forbidden":
+            opener = urllib.request.build_opener()
+            opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+            urllib.request.install_opener(opener)
+            file_path, _ = urllib.request.urlretrieve(file_url, file_path)
+        else:
+            raise ValueError("This is broken")
     # stat_info = os.stat(file_path)
     # print('Downloaded', file_path, stat_info.st_size, 'bytes.')
     
@@ -93,41 +101,61 @@ def remove_files_from_dir(dir):
 #############################################################JUPYTER NOTEBOOK FUNCTIONS################################################################
 
 
-def make_keyword():
-    search_terms = []
-    punctuation = '!#$%&()*+,./:;<=>?@[\]^_`{|}~'
+def make_search_terms():
+    """
+    Helper function to parse csv file and get wikidata urls, painting names and museum names
+    """
+    paintings_to_parse = []
     with open('/root/UROP/item_no_image_with_names.csv') as csv_file:
+    # with open('/Users/anisha/UROP/item_no_image_with_names.csv') as csv_file:
+
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
-            elif line_count > 100393:
-                # term = "+".join([row[1].translate(str.maketrans('', '', string.punctuation)), row[3].translate(str.maketrans('', '', punctuation)), row[5].translate(str.maketrans('', '', punctuation))])
-                term = "+".join(row[1].translate(str.maketrans('', '', string.punctuation)))
-                term = term.replace(" ", "+").strip("+")
-                search_terms.append(term)
-                line_count += 1
             else:
+                painting = [row[0], row[1], row[5]]
                 line_count += 1
+                paintings_to_parse.append(painting)
 
 
     print(f'Processed {line_count} lines.')
-    return search_terms
+    return paintings_to_parse
 
 
-def get_link_from_html(query):
+def get_ref_from_wiki(wikilink):
+    ref_url = 'P854'
+    url = wikilink[0]
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    soup_dict = json.loads(str(soup))
+    entity_key = list(soup_dict["entities"].keys())[0]
+    pvalues = soup_dict["entities"][entity_key]["claims"].keys()
+    for p in pvalues:
+        deconstructed = soup_dict["entities"][entity_key]["claims"][p][0]
+        keys = list(deconstructed.keys())
+        if 'references' in keys:
+            for reference in deconstructed['references']:
+                payload_keys = list(reference['snaks'].keys())
+                if ref_url in payload_keys:
+
+                    # print(reference['snaks'][ref_url][0]['datavalue']['value'])
+                    return reference['snaks'][ref_url][0]['datavalue']['value']
+                
+
+
+def get_image_from_html(data):
     """
-    Helper function to get first image url from bing response based on query
+    Helper function to get first image from html of a museum website
     params:
-        query: bing image search query
-    returns:
-        image url if available
-        else -1
+        data: [ref_url, painting name, museum_name]
     """
+    image_name = data[1]
+    museum = data[2]
     user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-    url = "http://www.bing.com/images/search?q=" + urllib.parse.quote(query) +"+painting+painting" + "&FORM=HDRSC2" 
+    url = data[0]
     headers = {'User-Agent': user_agent, }
     request = urllib.request.Request(url, None, headers)
     html = urllib.request.urlopen(request).read()
@@ -137,40 +165,80 @@ def get_link_from_html(query):
         print("Could not parse HTML response")
 
     try:
-        image_data = soup.find_all('li', {"data-idx": "1"})
-        a_data = image_data[0].find('a')
-        m_data = eval(a_data["m"])
-        if isinstance(m_data, dict):
-            murl = m_data["murl"]
-        return murl
+        # image_data = soup.find_all('li', {"data-idx": "1"})
+        body = soup.find('body')
+        images = (body.find_all('img'))
+        # print(images)
+        
+        for i in range(len(images)):
+            try:
+                if museum == "Museum of Modern Art":
+                    image_link = "http://www.moma.org" + images[i]['src'] 
+                elif museum == "Tate" and i in (0, 1):
+                    continue
+                elif museum == "Philadelphia Museum of Art" and i in (0, 1):
+                    continue
+                elif museum == "Cincinnati Art Museum" and i in (0,1):
+                    continue
+                elif museum == "Städel Museum":
+                    image_link = "https://sammlung.staedelmuseum.de/" + images[i]['src']
+                elif museum == "San Francisco Museum of Modern Art" and i == 0:
+                    continue
+                elif museum == 'National Galleries of Scotland' and i in range(0, 10):
+                    continue
+
+                else:
+                    image_link = images[i]['src']
+                
+                success = download_image((image_link, image_name, museum))
+                
+                if success:
+
+                    break
+                else:
+                    continue
+            except:
+                pass
+
     except:
-        pass
+        print("download of image failed")
     
-    return -1
 
-
-
-def download_image(query):
+def download_image(image_info):
     working_dir = "/mnt/restofallpaintings"
-    # working_dir= "/Users/anisha/UROP"
-    url = get_link_from_html(query)
-    if url != -1:
-        file_name = query + ".jpg"
-        try:
-            download_file_from_url(url, file_name, save_directory=working_dir)  
-            with open('/root/UROP/metadata.csv', 'a', newline='') as meta:
-                writer = csv.writer(meta)
-                writer.writerow([query, url])
-        except:
-            pass
+    # working_dir= "/Users/anisha/UROP/images"
+    url = image_info[0]
+    file_name = image_info[1] + ".jpg"
+    try:
+        download_file_from_url(url, file_name, save_directory=working_dir)  
+        # with open('/root/UROP/metadata.csv', 'a', newline='') as meta:
+        #     writer = csv.writer(meta)
+        #     writer.writerow([query, url])
+        return True
+    except:
+        # print("couldnt download image")
+        pass
+
+
+def top_fn(url_name_museum):
+    url = url_name_museum[0]
+    name = url_name_museum[1]
+    museum = url_name_museum[2]
+    ref_url = get_ref_from_wiki((url, museum))
+
+    get_image_from_html((ref_url, name, museum))
+
 
 
 def run():
     pool = Pool()
-    search_terms = make_keyword()
-    pool.map(download_image, search_terms)
+    search_terms = make_search_terms()
+    pool.map(top_fn, search_terms)
 
 # print(len(terms))
 # download_image(terms[0])
 run()
-
+# download_image("test")
+# get_link_from_html("https://www.museothyssen.org/en/collection/artists/delvaux-paul/viaduct")
+# make_search_terms()
+# get_ref_from_wiki("http://www.wikidata.org/entity/Q66016628")
